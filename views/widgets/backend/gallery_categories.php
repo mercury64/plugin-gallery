3b18da4f2205281a49e12ee46e7e2243fcb621b7
Butschster <butschster@gmail.com>
2014-03-19 13:58:00 +0400

    Добавлены дополнительные опции в виджет вывода категорий

--- views/widgets/backend/gallery_categories.php
+++ views/widgets/backend/gallery_categories.php
@@ -1,12 +1,34 @@
 <div class="widget-content">
-	<?php
-		echo Bootstrap_Form_Element_Control_Group::factory(array(
-			'element' => Bootstrap_Form_Element_Select::factory(array(
-				'name' => 'category_id', 'options' => $categories
-			))
-			->attributes('class', Bootstrap_Form_Element_Input::XXLARGE)
-			->selected($widget->category_id)
-			->label(__('Root category'))
-		));
-	?>
+	<div class="control-group">
+		<label class="control-label" for="id_ctx"><?php echo __('Category ID (Ctx)'); ?></label>
+		<div class="controls">
+			<?php echo Form::input( 'id_ctx', $widget->id_ctx, array(
+				'class' => 'input-small', 'id' => 'id_ctx'
+			) ); ?>
+		</div>
+	</div>
+	
+	<div class="control-group">
+		<label class="control-label" for="path_ctx"><?php echo __('Category path (Ctx)'); ?></label>
+		<div class="controls">
+			<?php echo Form::input( 'path_ctx', $widget->path_ctx, array(
+				'class' => 'input-small', 'id' => 'path_ctx'
+			) ); ?>
+		</div>
+	</div>
+	
+	<hr />
+	<?php echo Bootstrap_Form_Element_Control_Group::factory(array(
+		'element' => Bootstrap_Form_Element_Select::factory(array(
+			'name' => 'category_id', 'options' => $categories
+		))
+		->attributes('class', Bootstrap_Form_Element_Input::XXLARGE)
+		->selected($widget->category_id)
+		->label(__('Root category'))
+	)); ?>
+	<div class="control-group">
+		<div class="controls">
+			<label class="checkbox"><?php echo Form::checkbox('with_images', 1, $widget->with_images); ?> <?php echo __('Show only categories with attached image'); ?></label>
+		</div>
+	</div>
 </div>
